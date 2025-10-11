ef4028fd2cebaac520daa5f30a69caafb27c083c
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-10-11 17:25:01 +0300

    added Routes and making pages Foldes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage"
 
 
 function App() {
@@ -5,7 +7,15 @@ function App() {
 
   return (
     <div>
-
+       {/* header */}
+        <main>
+          {/* routes */}
+          <Routes>
+             {/* public route */}
+             <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+       {/* Footer */}
     </div>
   )
 }
