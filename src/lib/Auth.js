5d3ad4a394e10798b4a04c4a03fcae296e7a82e8
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-10-12 10:16:05 +0300

    added sign up functionality

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -9,4 +9,28 @@ export const signUp = async (email , password , username="")=>{
         })
          
       console.log(data);  
+
+    // we check our session 
+     const { data : sessionData } = await supabase.auth.getSession()
+      
+     if(!sessionData?.session){
+         console.log('no session yet');
+         return
+     }
+    
+     const displayName = username || data?.user.email.split('@')[0]
+    //  we insert the username 
+    const {data :profiledata , error : profileError } = await supabase
+            .from('users')
+            .insert({
+                id:data.user.id,
+                username:displayName,
+                avatar_url :null
+            })
+   
+        if(profileError){
+           console.log('profile insert SignUp',error);
+        }else{
+            console.log('success profile signUp:',profiledata);
+        } 
 }
