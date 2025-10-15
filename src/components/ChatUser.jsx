0211717a7a998d996c947ed85417988de9810871
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-10-15 09:53:27 +0300

    addad mobile style in chat

--- src/components/ChatUser.jsx
+++ src/components/ChatUser.jsx
@@ -1,36 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
+import { IoSend } from 'react-icons/io5'
+import { Link } from 'react-router'
 
 const ChatUser = ({userInfo}) => {
 
-   
+   const [message , setMassege]= useState('')
 
+    const handleSubmit = (e)=>{
+       e.preventDefault();
 
+       console.log(message);
+    }
 
 
 
   return (
     <div className='bg-white '>
-       <div className='flex flex-col min-h-screen'>
+       <div className='flex flex-col min-h-screen '>
          {/* Top  */}
-           <div className='h-15 flex items-center p-6'>
-             {/* profile */}
-              <img src={userInfo.avatar_url} alt="profile" className='h-13 w-13 border-1 border-gray-200 rounded-full' />
-             <span className='text-lg font-medium text-blue-400 mx-3'>{userInfo.username}</span>
+          <div className=''>
+            <div className='h-15 flex items-center p-6 '>
+              {/* profile */}
+              <img src={userInfo.avatar_url} alt="profile" className='h-13 w-13 border-1 border-gray-200 rounded-full '/>
+              <span className='text-lg font-medium text-blue-400 mx-3'>{userInfo.username}</span>
               <span className='block w-3 h-3 mt-0.5 rounded-full bg-green-500'></span> 
-           </div>
+            </div>
             <div className='bg-gray-300 h-0.5  rounded-md my-1'></div>
-
+          </div>
+           
             {/*Messagess text  */}
             <div className=' flex-1'>
                 Cool
             </div>
 
             {/* button */}
-            <div className='h-20 bg-gray-100'>
-                <form>
-                   <input type="text" />
-                   <button type='submit'>
-
+            <div className='h-20 bg-gray-100 flex justify-center items-center sticky bottom-0'>
+                <form onSubmit={handleSubmit} className='w-full p-2'>
+                   <input type="text" 
+                     className='p-2 w-6/7 text-lg bg-white focus:outline-none border-1 rounded-md border-gray-500'
+                     onChange={(e)=>setMassege(e.target.value)}
+                     value={message}
+                   />
+                   <button type='submit' className='ml-2 '>
+                        <IoSend  className='text-xl'/>
                    </button>
                 </form>
             </div>
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,118 +1,148 @@
-import React, { useEffect, useState } from 'react'
-import supabase from '../lib/supabase'
-import { useAuth } from '../context/AuthContext'
-import { FaUser } from 'react-icons/fa'
-import { Link, useParams } from 'react-router'
-import { MdOutlineEdit } from 'react-icons/md'
-import ChatUser from '../components/ChatUser'
+import React, { useEffect, useState } from "react";
+import supabase from "../lib/supabase";
+import { useAuth } from "../context/AuthContext";
+import { FaArrowLeft, FaUser } from "react-icons/fa";
+import { Link, useParams } from "react-router";
+import { MdOutlineEdit } from "react-icons/md";
+import ChatUser from "../components/ChatUser";
+import { FiArrowLeft } from "react-icons/fi";
 
 const ChatPage = () => {
+  const [currentUsers, setCurrentUsers] = useState([]);
+  const [message, setMessage] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [isOpen, setisOpen] = useState(false);
 
-    const [currentUsers , setCurrentUsers]= useState([])
-    const [message , setMessage]= useState('')
-    const [userData , setUserData] = useState(null) 
+  const { user, profile, isLoading } = useAuth();
 
+  const { id } = useParams();
 
-   const {user , profile , isLoading} = useAuth()
-    
-    const {id} = useParams()
-     
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("users")
+          .select("*")
+          .neq("id", user.id);
 
-   useEffect(()=>{
-       const fetchUser = async ()=>{
-          try {
-            
-            const {data, error } = await supabase
-             .from('users')
-             .select('*')
-             .neq('id', user.id) 
-            
-            if(error) throw error
-            
-            setCurrentUsers(data || [])
-            // console.log(data);
-          } catch (error) {
-            console.error(error);
+        if (error) throw error;
 
-          }
-       }
-       fetchUser()
-   },[])
-
-    const handleClick = (current)=>{
-        setUserData(current)
-    }
-  useEffect(()=>{
-      if(userData){
-          console.log('userData:',userData);
+        setCurrentUsers(data || []);
+        // console.log(data);
+      } catch (error) {
+        console.error(error);
       }
-  },[userData])
+    };
+    fetchUser();
+  }, []);
 
-   
+  const handleClick = (current) => {
+    setUserData(current);
+    setisOpen(true)
+  };
+ 
 
-    if(isLoading){
-       return (
-          <div className="flex justify-center items-center min-h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500"></div>
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500"></div>
       </div>
-       )
-    }
+    );
+  }
 
-   
 
 
   return (
-    <div className='min-h-screen max-w-4xl mx-auto'>
-      <div className=' grid grid-cols-3'>
-         {/* left side */}
-          <div className='bg-gray-100 min-h-screen'>
-              <div >
-                {/* profile */}
-                 <div className='flex justify-between items-center p-2 sm:p-4'>
-                    <div className='flex flex-col sm:flex-row sm:space-x-3 sm:items-center'>
-                      {
-                        profile ? <img src={profile?.avatar_url} alt='profileImage' 
-                          className='h-15 w-15 sm:h-20 sm:w-20 rounded-full border-1 border-gray-200  '
-                        />
-                        :<FaUser className='text-2xl text-rose-500' />
-                      }
-                      <span className='text-lg text-blue-500 font-medium'>{profile?.username}</span>
-                   </div>
-                    <Link to={'/profile'}>
-                      <MdOutlineEdit className='text-xl text-gray-600' />
-                    </Link>
-                 </div>
-                   <div className='bg-gray-400 p-0.5 rounded-md my2'></div>
-                  {/* current users  */}
-                  <h2 className='text-xl font-medium '>Current Users</h2>
-                  <div className='p-2 sm:p-4 flex flex-col space-y-3'>
-                     {
-                      currentUsers.map((current)=>(
-                        <Link key={current.id} 
-                           onClick={()=>handleClick(current)} 
-                         >
-                           <div className='flex space-x-1 items-center'>
-                              <img src={current.avatar_url} alt="userimage" className='h-15 w-15 rounded-full border-1 border-gray-200 '/>
-                              
-                             <div className='flex justify-between'>
-                                   <span className='text-blue-500 text-md font-medium'>{current.username}</span>
-                               </div>
-                           </div>
-                        </Link> 
-                      ))
-                     }
-                  </div> 
+    <div className="min-h-screen max-w-4xl mx-auto">
+      <div className="hidden  sm:grid sm:grid-cols-3">
+        {/* left side */}
+        <div className="bg-gray-100 min-h-screen">
+          <div>
+            {/* profile */}
+            <div className="flex justify-between items-center p-2 sm:p-4">
+              <div className="flex flex-col sm:flex-row sm:space-x-3 sm:items-center">
+                {profile ? (
+                  <img
+                    src={profile?.avatar_url}
+                    alt="profileImage"
+                    className="h-15 w-15 sm:h-20 sm:w-20 rounded-full border-1 border-gray-200  "
+                  />
+                ) : (
+                  <FaUser className="text-2xl text-rose-500" />
+                )}
+                <span className="text-lg text-blue-500 font-medium">
+                  {profile?.username}
+                </span>
               </div>
+              <Link to={"/profile"}>
+                <MdOutlineEdit className="text-xl text-gray-600" />
+              </Link>
+            </div>
+            <div className="bg-gray-400 p-0.5 rounded-md my2"></div>
+            {/* current users  */}
+            <h2 className="text-xl font-medium ">Current Users</h2>
+            <div className="p-2 sm:p-4 flex flex-col space-y-3">
+              {currentUsers.map((current) => (
+                <div key={current.id} onClick={() => handleClick(current)}>
+                  <div className="flex space-x-1 items-center">
+                    <img
+                      src={current.avatar_url}
+                      alt="userimage"
+                      className="h-15 w-15 rounded-full border-1 border-gray-200 "
+                    />
+
+                    <div className="flex justify-between">
+                      <span className="text-blue-500 text-md font-medium">
+                        {current.username}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-         {/* right side */}
-          <div className='col-span-2 '>
-              {
-                userData && <ChatUser userInfo={userData}/>
-              }   
+        </div>
+        {/* right side */}
+        <div className="col-span-2 ">
+          {userData && <ChatUser userInfo={userData} />}
+        </div>
+      </div>
+      {/* mobile design  */}
+      <div className="sm:hidden">
+        {isOpen ? (
+          <div className="relative"> 
+            <FiArrowLeft className="text-md absolute top-6 left-0 z-20 cursor-pointer" 
+              onClick={()=> setisOpen(false)}
+            />
+           <ChatUser userInfo={userData}/>
           </div>
+         
+        ) : (
+          <div className="p-6 ">
+            <div className="p-2 sm:p-4 flex flex-col space-y-6">
+              {currentUsers.map((current) => (
+                <div key={current.id} onClick={() => handleClick(current)}>
+                  <div className="flex space-x-3 items-center">
+                    <img
+                      src={current.avatar_url}
+                      alt="userimage"
+                      className="h-15 w-15 rounded-full border-1 border-gray-200 "
+                    />
+
+                    <div className="flex justify-between">
+                      <span className="text-blue-500 text-md font-medium">
+                        {current.username}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
