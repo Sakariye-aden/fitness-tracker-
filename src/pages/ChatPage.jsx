4e57a37438e101741b4da9f2940e19273e586aa0
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-10-23 10:24:48 +0300

    added style

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -6,6 +6,8 @@ import { Link, useParams } from "react-router";
 import { MdOutlineEdit } from "react-icons/md";
 import ChatUser from "../components/ChatUser";
 import { FiArrowLeft } from "react-icons/fi";
+import { IoChatboxEllipses } from "react-icons/io5";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const ChatPage = () => {
   const [currentUsers, setCurrentUsers] = useState([]);
@@ -60,7 +62,7 @@ const ChatPage = () => {
     <div className="min-h-screen max-w-4xl mx-auto">
       <div className="hidden  sm:grid sm:grid-cols-3">
         {/* left side */}
-        <div className="bg-gray-100 min-h-screen">
+        <div className="bg-slate-800  min-h-screen">
           <div>
             {/* profile */}
             <div className="flex justify-between items-center p-2 sm:p-4">
@@ -69,17 +71,17 @@ const ChatPage = () => {
                   <img
                     src={profile?.avatar_url}
                     alt="profileImage"
-                    className="h-15 w-15 sm:h-20 sm:w-20 rounded-full border-1 border-gray-200  "
+                    className="h-12 w-12 sm:h-20 sm:w-20 rounded-full border-1 border-gray-200  "
                   />
                 ) : (
-                  <FaUser className="text-2xl text-rose-500" />
+                  <FaUser className="text-2xl text-white" />
                 )}
-                <span className="text-lg text-blue-500 font-medium">
+                <span className="text-lg text-white font-medium">
                   {profile?.username}
                 </span>
               </div>
               <Link to={"/profile"}>
-                <MdOutlineEdit className="text-xl text-gray-600" />
+                <MdOutlineEdit className="text-xl text-white" />
               </Link>
             </div>
             <div className="bg-gray-400 p-0.5 rounded-md my2"></div>
@@ -96,7 +98,7 @@ const ChatPage = () => {
                     />
 
                     <div className="flex justify-between">
-                      <span className="text-blue-500 text-md font-medium">
+                      <span className="text-white text-md font-medium">
                         {current.username}
                       </span>
                     </div>
@@ -112,7 +114,7 @@ const ChatPage = () => {
         </div>
       </div>
       {/* mobile design  */}
-      <div className="sm:hidden">
+      <div className="sm:hidden bg-slate-900 min-h-screen">
         {isOpen ? (
           <div className="relative"> 
             <FiArrowLeft className="text-md absolute top-6 left-0 z-20 cursor-pointer" 
@@ -123,18 +125,25 @@ const ChatPage = () => {
          
         ) : (
           <div className="p-6 ">
-            <div className="p-2 sm:p-4 flex flex-col space-y-6">
+             <div className="py-4 flex justify-between items-center">
+               <div className="flex items-center space-x-2 text-white ">
+                 <IoChatboxEllipses  className="text-4xl"/>
+                 <span className="font-medium text-xl  pb-2">Chatapp</span>
+               </div>
+               <BsThreeDotsVertical className="text-white text-xl" />
+             </div>
+            <div className="p-2 sm:p-4 flex flex-col space-y-6 mt-2">
               {currentUsers.map((current) => (
                 <div key={current.id} onClick={() => handleClick(current)}>
                   <div className="flex space-x-3 items-center cursor-pointer">
                     <img
                       src={current.avatar_url}
                       alt="userimage"
-                      className="h-15 w-15 rounded-full border-1 border-gray-200 "
+                      className="h-12 w-12 rounded-full border-1 border-gray-200 "
                     />
 
                     <div className="flex justify-between">
-                      <span className="text-blue-500 text-md font-medium">
+                      <span className="text-white text-md font-medium">
                         {current.username}
                       </span>
                     </div>
